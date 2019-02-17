4337d3701273a03e04d4b3690a5ebe11a7516aa7
taqreez <taqreez.ali@gmail.com>
2019-02-17 22:23:51 +0530

    Initial project checkin

---
+++ src/main/java/com/taqy/learning/taqypetclinic/TaqyPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.taqy.learning.taqypetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaqyPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaqyPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taqy/learning/taqypetclinic/TaqyPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.taqy.learning.taqypetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaqyPetClinicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
