9c18d5020af636b46d668c243b45c8af72763768
taqreez <taqreez.ali@gmail.com>
2019-02-21 01:37:09 +0530

    Added id to models Fixes #10

--- pet-clinic-data/src/main/java/com/taqy/learning/taqypetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/taqy/learning/taqypetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.taqy.learning.taqypetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
 
     private PetType petType;
     private Owner owner;
