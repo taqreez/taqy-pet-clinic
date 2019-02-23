c7450ec946e82df19a391986b544311805f67851
taqreez <taqreez.ali@gmail.com>
2019-02-23 13:36:10 +0530

    Refactored interfaces #11

--- pet-clinic-data/src/main/java/com/taqy/learning/taqypetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/taqy/learning/taqypetclinic/services/OwnerService.java
@@ -2,11 +2,6 @@ package com.taqy.learning.taqypetclinic.services;
 
 import com.taqy.learning.taqypetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long>{
     Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/com/taqy/learning/taqypetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/taqy/learning/taqypetclinic/services/VetService.java
@@ -2,13 +2,8 @@ package com.taqy.learning.taqypetclinic.services;
 
 import com.taqy.learning.taqypetclinic.model.Vet;
 
-import java.util.Set;
-
 /**
  * Create by TA on 2019-02-21
  */
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet pet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
 }
